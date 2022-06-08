87c75692d827b6c97e9f8ed0b8fb90bcd7e5007b
julinbestina <julin.bestina@yahoo.com>
2022-06-08 17:00:55 +0530

    Modified the code to Calculate EmpWage for multiple companies

--- EmpWageCompute.java
+++ EmpWageCompute.java
@@ -1,10 +1,23 @@
 package com.bridgelabz.empwage;
 
+import java.util.ArrayList;
+
 public class EmpWageCompute {
-    public static int calculateWage(int totalWorkingHour, int totalWorkingDay, int empWagePerHour) {
-        int empHour;
-        while (totalWorkingHour <= 100 && totalWorkingDay < 20) {
-            totalWorkingDay++;
+    private final int totalWorkingHourOfFirm;
+    private final int totalWorkingDayOfFirm;
+    private final int empWagePerHourOfFirm;
+    private String firmName;
+
+    public EmpWageCompute(int totalWorkingHourOfFirm, int totalWorkingDayOfFirm, int empWagePerHourOfFirm, String firmName) {
+        this.totalWorkingHourOfFirm = totalWorkingHourOfFirm;
+        this.totalWorkingDayOfFirm = totalWorkingDayOfFirm;
+        this.empWagePerHourOfFirm = empWagePerHourOfFirm;
+        this.firmName = firmName;
+    }
+
+    public int calculateWage() {
+        int empHour, totalWorkingHour = 0, totalWorkingDay = 1;
+        while (totalWorkingHour <= totalWorkingHourOfFirm && totalWorkingDay <= totalWorkingDayOfFirm) {
             int empCheck = (int) (Math.random() * 10) % 3;
             switch (empCheck) {
                 case 1:
@@ -18,18 +31,31 @@ public class EmpWageCompute {
             }
 
             totalWorkingHour += empHour;
-            System.out.println("Day: " + totalWorkingDay + " Working Hour: " + totalWorkingHour);
+            totalWorkingDay++;
         }
 
-        int totalWage = (totalWorkingHour * empWagePerHour);
+        int totalWage = totalWorkingHour * empWagePerHourOfFirm;
         return totalWage;
     }
 
     public static void main(String[] args) {
-        int totalWorkingHour = 0, totalWorkingDay = 0;
-        int empWagePerHour = 20, totalWage;
-        totalWage = EmpWageCompute.calculateWage(totalWorkingHour, totalWorkingDay, empWagePerHour);
-        System.out.println("Total Emoloyee Wage: " + totalWage);
+
+        ArrayList<EmpWageCompute> firm = new ArrayList<EmpWageCompute>();
+        EmpWageCompute Dell = new EmpWageCompute(100, 20, 200, "Dell");
+        EmpWageCompute IBM = new EmpWageCompute(120, 25, 500, "IBM");
+        EmpWageCompute HCL = new EmpWageCompute(100, 22, 400, "HCL");
+        EmpWageCompute TCS = new EmpWageCompute(150, 24, 700, "TCS");
+
+        firm.add(Dell);
+        firm.add(IBM);
+        firm.add(HCL);
+        firm.add(TCS);
+
+        for (EmpWageCompute l : firm) {
+            System.out.println(l.firmName + ":" + l.totalWorkingHourOfFirm + "," + l.totalWorkingDayOfFirm + "," + l.empWagePerHourOfFirm + "," +
+                    +l.calculateWage());
+        }
+
     }
 }
 
