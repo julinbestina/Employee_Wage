bcb3e53ff65f2a75e53ebfeaf36ef267b33925c7
julinbestina <julin.bestina@yahoo.com>
2022-06-08 15:09:13 +0530

    Refactored the code to have a Class method to compute Emp Wage

--- EmpWageCompute.java
+++ EmpWageCompute.java
@@ -1,32 +1,34 @@
 package com.bridgelabz.empwage;
 
 public class EmpWageCompute {
-    public static void main(String[] args) {
-        int empHour, empCheck, totalWorkingHour = 0, totalWorkingDay = 0;
-        int empWagePerHour = 20, totalWage;
-
-
-        while( totalWorkingHour <= 100 && totalWorkingDay < 20) {
+    public static int calculateWage(int totalWorkingHour, int totalWorkingDay, int empWagePerHour) {
+        int empHour;
+        while (totalWorkingHour <= 100 && totalWorkingDay < 20) {
             totalWorkingDay++;
-                empCheck = (int) (Math.random() * 10) % 3;
-                switch (empCheck) {
-                    case 1:
-                        empHour = 8;
-                        break;
-                    case 2:
-                        empHour = 4;
-                        break;
-                    default:
-                        empHour = 0;
-                }
-
-                totalWorkingHour += empHour;
-                System.out.println("Day: " + totalWorkingDay + " Working Hour: " + totalWorkingHour);
+            int empCheck = (int) (Math.random() * 10) % 3;
+            switch (empCheck) {
+                case 1:
+                    empHour = 8;
+                    break;
+                case 2:
+                    empHour = 4;
+                    break;
+                default:
+                    empHour = 0;
             }
 
-            totalWage = (totalWorkingHour * empWagePerHour);
-        
+            totalWorkingHour += empHour;
+            System.out.println("Day: " + totalWorkingDay + " Working Hour: " + totalWorkingHour);
+        }
 
+        int totalWage = (totalWorkingHour * empWagePerHour);
+        return totalWage;
+    }
+
+    public static void main(String[] args) {
+        int totalWorkingHour = 0, totalWorkingDay = 0;
+        int empWagePerHour = 20, totalWage;
+        totalWage = EmpWageCompute.calculateWage(totalWorkingHour, totalWorkingDay, empWagePerHour);
         System.out.println("Total Emoloyee Wage: " + totalWage);
     }
 }
