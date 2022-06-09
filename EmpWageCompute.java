8d95b7ddcc8318ce06ddcfe43b314ee1d7e8e7be
julinbestina <julin.bestina@yahoo.com>
2022-06-09 14:42:06 +0530

    Refactored code with Function parameters for multiple companies

--- EmpWageCompute.java
+++ EmpWageCompute.java
@@ -1,23 +1,12 @@
 package com.bridgelabz.empwage;
 
-import java.util.ArrayList;
-
 public class EmpWageCompute {
-    private final int totalWorkingHourOfFirm;
-    private final int totalWorkingDayOfFirm;
-    private final int empWagePerHourOfFirm;
-    private String firmName;
+    public static void calculateWage(String firmName, int totalWorkingHourOfFirm, int totalWorkingDayOfFirm, int empWagePerHourOfFirm) {
 
-    public EmpWageCompute(int totalWorkingHourOfFirm, int totalWorkingDayOfFirm, int empWagePerHourOfFirm, String firmName) {
-        this.totalWorkingHourOfFirm = totalWorkingHourOfFirm;
-        this.totalWorkingDayOfFirm = totalWorkingDayOfFirm;
-        this.empWagePerHourOfFirm = empWagePerHourOfFirm;
-        this.firmName = firmName;
-    }
+        int empHour, totalWorkingHour = 0, totalWorkingDay = 0;
 
-    public int calculateWage() {
-        int empHour, totalWorkingHour = 0, totalWorkingDay = 1;
-        while (totalWorkingHour <= totalWorkingHourOfFirm && totalWorkingDay <= totalWorkingDayOfFirm) {
+        while (totalWorkingHour <= totalWorkingHourOfFirm && totalWorkingDay < totalWorkingDayOfFirm) {
+            totalWorkingDay++;
             int empCheck = (int) (Math.random() * 10) % 3;
             switch (empCheck) {
                 case 1:
@@ -31,31 +20,18 @@ public class EmpWageCompute {
             }
 
             totalWorkingHour += empHour;
-            totalWorkingDay++;
         }
 
         int totalWage = totalWorkingHour * empWagePerHourOfFirm;
-        return totalWage;
+        System.out.println("Company Name: " + firmName + ", " + "Total Wage: " + totalWage + ", " + ", " + "Working Days: " +
+                +totalWorkingDay + ", " + "Working Hour: " + totalWorkingHour + ", " + "Employee Wage per Hour: " + empWagePerHourOfFirm);
     }
 
     public static void main(String[] args) {
 
-        ArrayList<EmpWageCompute> firm = new ArrayList<EmpWageCompute>();
-        EmpWageCompute Dell = new EmpWageCompute(100, 20, 200, "Dell");
-        EmpWageCompute IBM = new EmpWageCompute(120, 25, 500, "IBM");
-        EmpWageCompute HCL = new EmpWageCompute(100, 22, 400, "HCL");
-        EmpWageCompute TCS = new EmpWageCompute(150, 24, 700, "TCS");
-
-        firm.add(Dell);
-        firm.add(IBM);
-        firm.add(HCL);
-        firm.add(TCS);
-
-        for (EmpWageCompute l : firm) {
-            System.out.println(l.firmName + ":" + l.totalWorkingHourOfFirm + "," + l.totalWorkingDayOfFirm + "," + l.empWagePerHourOfFirm + "," +
-                    +l.calculateWage());
-        }
-
+        calculateWage("TCS", 150, 24, 700);
+        calculateWage("HCL", 100, 22, 400);
+        calculateWage("IBM", 120, 25, 500);
+        calculateWage("Dell", 100, 20, 200);
     }
 }
-
